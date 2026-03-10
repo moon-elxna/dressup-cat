249d851477b35a610b7b9c07e228a3b1e602b09a
moon-elxna <kreinelina@gmail.com>
2026-03-10 21:53:06 +0100

    wip

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 //---main---
 const showcase = [
-    {index: 0, name: "accessories",   min: null, max: 2, current: []}, 
-    {index: 1, name: "top",           min: null, max: 3, current: []}, 
-    {index: 2, name: "bottom",        min: null, max: 3, current: []}, 
+    {name: "accessories",   min: null, max: 2, current: []}, 
+    {name: "top",           min: null, max: 3, current: []}, 
+    {name: "bottom",        min: null, max: 3, current: []}, 
 ]
-const arrows = ["right", "left"];
-const amount_of_showcase_img = 3;
+const arrow = ["right", "left"];
+const amount_showcase = 3;
 load_showcase_img();
-
 
 //---EventListeners---
 //all btns of showcase
 for(let i = 0; i < showcase.length; i++){
-    for(let j = 0; j < arrows.length; j++){
-        document.getElementById("showcase_btn_" + arrows[j] + "_" + showcase[i].name)
-        .addEventListener("click", function(){showcase_elements_arrow_buttons(i, arrows[j])});
+    for(let j = 0; j < arrow.length; j++){
+        document.getElementById("showcase_btn_" + arrow[j] + "_" + showcase[i].name)
+        .addEventListener("click", function(){showcase_elements_arrow_buttons(i, j)});
     }
 }
 
@@ -24,25 +23,24 @@ function load_showcase_img(){
     for(let i = 0; i < showcase.length; i++){
         showcase[i].min = 0;
         build_showcase_current(i); //set min img to 0
-        load_img(i, arrows[0]);
+        load_img(i, 0);
     }
     //builds up empty array
     function build_showcase_current(index){
-        for(let i = 0; i < amount_of_showcase_img; i++){
+        for(let i = 0; i < amount_showcase; i++){
             showcase[index].current.push(showcase[index].min + i)
         }
-        console.log("build " + showcase[index].current);
     }   
 }
 
-function showcase_elements_arrow_buttons(index, direction){
+function showcase_elements_arrow_buttons(index, arrow){
     //create local var from array
     const name = showcase[index].name;
     const min = showcase[index].min;
     const max = showcase[index].max;
     let current = showcase[index].current;
     //array loops image ids
-    if(direction === "right"){
+    if(arrow === 0){
         //remove first img
         document.getElementById("btn_" + name + String(current[0])).remove();
         //change array
@@ -68,41 +66,36 @@ function showcase_elements_arrow_buttons(index, direction){
             }
         }
     }
-    //set array
     showcase[index].current = current;
-    console.log("btns arr " + current);
-    //reload needed imgs
-    load_img(index, direction);
+    load_img(index, arrow);
 }
 
-function load_img(index, direction){
+function load_img(index, arrow){
     //create local var from array
     const name = showcase[index].name;
     const current = showcase[index].current;
     //find div by id
     const div = document.getElementById("showcase_img_" + name);
     //loop through 3 images of an element
-    for(let i = 0; i < amount_of_showcase_img; i++){
+    for(let i = 0; i < amount_showcase; i++){
         const num = String(current[i]);
         //check if img does not exist, then add the image
         if(document.getElementById(name + num) === null){ 
             //create and append btn w image
             const btn = document.createElement("button");
             btn.id = "btn_" + name + num;
-            btn.style.width = "150px"; btn.style.height = btn.style.width; 
+            btn.style.width = "156px"; btn.style.height = "108px"; 
             const img = document.createElement("img");
-            img.src = "assets/" + name + num + ".PNG";
+            img.src = "assets/showcase/" + name + num + ".PNG";
             img.alt = name;
             img.id = name + num;
-            img.style.width = "150px"; 
+            img.style.width = btn.style.width; img.style.height = btn.style.heigh;
             
-            if(direction === arrows[0]){
-                btn.append(img);
-                div.append(btn);
+            if(arrow === 0){
+                btn.append(img); div.append(btn);
             }
             else{
-                btn.prepend(img);
-                div.prepend(btn);
+                btn.prepend(img); div.prepend(btn);
             }   
         }
     }
