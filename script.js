eb933161e781982a31659b8f37dc5d4cec227dd4
moon-elxna <kreinelina@gmail.com>
2026-03-10 22:53:43 +0100

    wip

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //---main---
 const showcase = [
-    {name: "accessories",   min: null, max: 2, current: []}, 
-    {name: "top",           min: null, max: 3, current: []}, 
-    {name: "bottom",        min: null, max: 3, current: []}, 
+    {name: "accessories",   min: 0, max: 2, current: []}, 
+    {name: "top",           min: 0, max: 3, current: []}, 
+    {name: "bottom",        min: 0, max: 3, current: []}, 
 ]
 const arrow = ["right", "left"];
 const amount_showcase = 3;
@@ -18,20 +18,11 @@ for(let i = 0; i < showcase.length; i++){
 
 //---functions---
 function load_showcase_img(){
+    read_localstorage();
     //loads images for each element by loop through array and calling function
     for(let i = 0; i < showcase.length; i++){
-        showcase[i].min = 0;
-        build_showcase_current(i); //set min img to 0
         load_img(i, 0);
     }
-    //reads local storage for the dressup
-    read_localstorage();
-    //builds up empty array
-    function build_showcase_current(index){
-        for(let i = 0; i < amount_showcase; i++){
-            showcase[index].current.push(showcase[index].min + i)
-        }
-    } 
 }
 
 function showcase_elements_arrow_buttons(index, arrow){
@@ -68,6 +59,7 @@ function showcase_elements_arrow_buttons(index, arrow){
         }
     }
     showcase[index].current = current;
+    localStorage.setItem(name +".current", JSON.stringify(current));
     load_img(index, arrow);
 }
 
@@ -103,23 +95,31 @@ function load_img(index, arrow){
 function change_img(name, index){
     const source = "assets/dressup/" + name + String(index) + ".PNG";
     document.getElementById(name).src = source;
-    write_localstorage(name, source);
+    localStorage.setItem(name, source);
 }
 
 function read_localstorage(){
     for(let i = 0; i < showcase.length; i++){
         const name = showcase[i].name;
-        const source = localStorage.getItem(name);
+        //dressup images
+        let source = localStorage.getItem(name);
         if(source != null){
             document.getElementById(name).src = source;
         }   
         else{
-            document.getElementById(name).src = "assets/dressup/" + name + "0.PNG";
-            write_localstorage(name, source);
+            source = "assets/dressup/" + name + "0.PNG"
+            document.getElementById(name).src = source;
+            localStorage.setItem(name, source);
+        }
+        //showcase images
+        const array = JSON.parse(localStorage.getItem(name + ".current"))
+        if(array != null){
+            showcase[i].current = array;
+        }
+        else{
+            for(let j = 0; j < amount_showcase; j++){
+                showcase[i].current.push(showcase[i].min + j);
+            }
         }
     }
-}
-
-function write_localstorage(name, source){
-    localStorage.setItem(name, source);
 }
